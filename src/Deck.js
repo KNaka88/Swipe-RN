1d5c551efffb1591259295ec463098563667691b
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-09-12 16:33:00 -0700

    Made cards draggable

--- src/Deck.js
+++ src/Deck.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import React, { Component, useRef } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
 
 const Deck = props => {
+    const position = useRef(new Animated.ValueXY()).current;
+    const panResponder = React.useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            },
+            onPanResponderRelease: () => {}
+        })
+    ).current;
+
     const renderCards = () => props.data.map(item => {
             return props.renderCard(item);
         });
-        
+
     return (
-        <View>
+        <Animated.View 
+            style={position.getLayout()}
+            {...panResponder.panHandlers}>
             {renderCards()}
-        </View>
+        </Animated.View>
     );
 };
 
