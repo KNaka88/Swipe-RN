1a74e429864193e78ec67403de1fb7a47edb3fe4
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-09-12 17:48:08 -0700

    Rotate card

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,9 @@
 import React, { Component, useRef } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
-const Deck = props => {
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Deck = ({data, renderCard}) => {
     const position = useRef(new Animated.ValueXY()).current;
     const panResponder = React.useRef(
         PanResponder.create({
@@ -13,16 +15,37 @@ const Deck = props => {
         })
     ).current;
 
-    const renderCards = () => props.data.map(item => {
-            return props.renderCard(item);
+    const getCardStyle = () => {
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        }; 
+    };
+
+    const renderCards = () => data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={getCardStyle()}
+                        {...panResponder.panHandlers}   
+                    >
+                        {renderCard(item)}
+                    </Animated.View>
+                );
+            }
+            
+            return renderCard(item);
         });
 
     return (
-        <Animated.View 
-            style={position.getLayout()}
-            {...panResponder.panHandlers}>
+        <View>
             {renderCards()}
-        </Animated.View>
+        </View>
     );
 };
 
